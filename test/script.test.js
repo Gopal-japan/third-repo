a53f6a1cff1610e1468b74e8536df49394ff2386
demo project <dahaldahalg2054@gmail.com>
2024-11-22 14:17:38 +0900

    test updated

--- test/script.test.js
+++ test/script.test.js
@@ -1,5 +1,13 @@
-const { greetUser } = require('../script');
+const { add, subtract } = require('../script');
 
-test('greetUser returns the correct greeting message', () => {
-  expect(greetUser()).toBe("Hello, welcome to the site!");
+test('add should return the sum of two numbers', () => {
+  expect(add(2, 3)).toBe(5);
+  expect(add(-1, 1)).toBe(0);
+  expect(add(-2, -3)).toBe(-5);
+});
+
+test('subtract should return the difference between two numbers', () => {
+  expect(subtract(5, 3)).toBe(2);
+  expect(subtract(3, 5)).toBe(-2);
+  expect(subtract(-1, 1)).toBe(-2);
 });
